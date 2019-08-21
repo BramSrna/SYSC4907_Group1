8ee24bf7c34b878cb18d1cb034b35563ed24643b
Bram Srna <bramsrna@cmail.carleton.ca>
2019-08-20 21:48:02 -0400

    [task] Remove image-picker 	- Needs to be readded

--- Application/pages/AddItemPage.js
+++ Application/pages/AddItemPage.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Text, View, TextInput, Picker, Button, Image } from "react-native";
-import ImagePicker from "react-native-image-picker";
 import styles from "./pageStyles/AddItemPageStyle";
 
 class AddItemPage extends Component {
@@ -10,42 +9,8 @@ class AddItemPage extends Component {
     this.state = {itemName: "Item Name",
                   itemDepartment: "Department",
                   storeName: "Store Name",
-                  aisleNum: "Aisle Number",
-                  filePath: {},};
+                  aisleNum: "Aisle Number",};
   }
-
-  chooseFile = () => {
-    var options = {
-      title: "Select Image",
-      customButtons: [
-        {name: "customOptionKey",
-         title: "Choose Photo from Custom Option"},
-      ],
-      storageOptions: {
-        skipBackup: true,
-        path: "images",
-      },
-    };
-
-    ImagePicker.showImagePicker(options, response => {
-      console.log("Response = ", response);
-      if (response.didCancel) {
-        console.log("User cancelled image picker");
-      } else if (response.error) {
-        console.log("ImagePicker Error: ", response.error);
-      } else if (response.customButton) {
-        console.log("User tapped custom button: ", response.customButton);
-        alert(response.customButton);
-      } else {
-        let source = response;
-        // You can also display the image using data:
-        // let source = { uri: "data:image/jpeg;base64," + response.data};
-        this.setState({
-          filePath: source,
-        });
-      }
-    });
-  };
 
   render() {
     return (
@@ -67,26 +32,6 @@ class AddItemPage extends Component {
               value={this.state.itemName}
             />
           </View>
-        </View>
-
-        <View style = {{flex: 1}}>
-          {/*<Image
-          source={{ uri: this.state.filePath.path}}
-          style={{width: 100, height: 100}} />*/}
-          <Image
-            source={{
-              uri: "data:image/jpeg;base64," + this.state.filePath.data,
-            }}
-            style={{ width: 100, height: 100}}
-          />
-          <Image
-            source={{ uri: this.state.filePath.uri }}
-            style={{ width: 250, height: 250 }}
-          />
-          <Text style={{ alignItems: "center" }}>
-            {this.state.filePath.uri}
-          </Text>
-          <Button title="Choose File" onPress={this.chooseFile.bind(this)} />
         </View>
 
         <View style={{flexDirection: "row"}}>
