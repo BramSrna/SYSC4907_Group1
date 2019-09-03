8b276a3207d59f92c5fee17a670567c51006c940
Bram Srna <bramsrna@cmail.carleton.ca>
2019-09-02 23:43:55 -0400

    Finalize addItem page layout

--- Application/pages/AddItemPage.js
+++ Application/pages/AddItemPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, View, TextInput, Picker, Button, Image } from "react-native";
+import { Text, View, TextInput, Picker, Button, Image, TouchableHighlight } from "react-native";
 import styles from "./pageStyles/AddItemPageStyle";
 
 class AddItemPage extends Component {
@@ -12,94 +12,98 @@ class AddItemPage extends Component {
                   aisleNum: "Aisle Number",};
   }
 
+  buttonListener = buttonId => {
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.blackText}>Add Item:</Text>
+        <View style={styles.topContainer}>
+          <Text style={styles.whiteHeaderText}>Add Item:</Text>
+        </View>
 
-        <View style={{flexDirection: "row"}}>
-          <View style = {{flex: 1}}>
-            <Text style={styles.blackText}>Item Name: </Text>
+        <View style={styles.midContainer}>
+          <View style={styles.rowSorter}>
+            <View style = {{flex: 1}}>
+              <Text style={styles.whiteText}>Item Name: </Text>
+            </View>
+
+            <View style = {{flex: 1}}>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.itemName}
+              />
+            </View>
           </View>
 
-          <View style = {{flex: 1}}>
-            <TextInput
-              style={{height: 40, 
-                      borderColor: "gray", 
-                      borderWidth: 1,
-                      justifyContent: "flex-end"}}
-              onChangeText={(text) => this.setState({text})}
-              value={this.state.itemName}
-            />
+          <View style={styles.rowSorter}>
+            <View style = {{flex: 1}}>
+              <Text style={styles.whiteText}>Item Department: </Text>
+            </View>
+            
+            <View style = {{flex: 1}}>
+              <Picker
+                selectedValue={this.state.language}
+                style={styles.picker}
+                onValueChange={(itemValue, itemIndex) =>
+                  this.setState({language: itemValue})
+                }>
+                <Picker.Item label="Bakery" value="BAKERY" />
+                <Picker.Item label="Beer" value="BEER" />
+                <Picker.Item label="Bulk" value="BULK" />
+                <Picker.Item label="Cheese" value="CHEESE" />
+                <Picker.Item label="Coffee And Tea" value="COFFEE_AND_TEA" />
+                <Picker.Item label="Flowers and Floral Arrangements" value="FLOWERS_AND_FLORAL_ARRANGEMENTS" />
+                <Picker.Item label="Grocery" value="GROCERY" />
+                <Picker.Item label="Meat and Poultry" value="MEAT_AND_POULTRY" />
+                <Picker.Item label="Prepared Foods" value="PREPARED_FOODS" />
+                <Picker.Item label="Produce" value="PRODUCE" />
+                <Picker.Item label="Seafood" value="SEAFOOD" />
+                <Picker.Item label="Wine" value="WINE" />
+                <Picker.Item label="Whole Body" value="WHOLE_BODY" />
+                <Picker.Item label="Pets" value="PETS" />
+              </Picker>
+            </View>
+          </View>
+
+          <View style={styles.rowSorter}>
+            <View style = {{flex: 1}}>
+              <Text style={styles.whiteText}>Store Name: </Text>
+            </View>
+
+            <View style = {{flex: 1}}>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.storeName}
+              />
+            </View>
+          </View>
+
+          <View style={styles.rowSorter}>
+            <View style = {{flex: 1}}>
+              <Text style={styles.whiteText}>Aisle Number: </Text>
+            </View>
+
+            <View style = {{flex: 1}}>
+              <TextInput
+                style={styles.textInput}
+                keyboardType = "numeric"
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.aisleNum}
+              />
+            </View>
           </View>
         </View>
 
-        <View style={{flexDirection: "row"}}>
-          <View style = {{flex: 1}}>
-            <Text style={styles.blackText}>Item Department: </Text>
-          </View>
-          
-          <View style = {{flex: 1}}>
-            <Picker
-              selectedValue={this.state.language}
-              style={{height:  40,
-                      borderColor: "gray",
-                      borderWidth: 1,
-                      justifyContent: "flex-end"}}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({language: itemValue})
-              }>
-              <Picker.Item label="Bakery" value="BAKERY" />
-              <Picker.Item label="Beer" value="BEER" />
-              <Picker.Item label="Bulk" value="BULK" />
-              <Picker.Item label="Cheese" value="CHEESE" />
-              <Picker.Item label="Coffee And Tea" value="COFFEE_AND_TEA" />
-              <Picker.Item label="Flowers and Floral Arrangements" value="FLOWERS_AND_FLORAL_ARRANGEMENTS" />
-              <Picker.Item label="Grocery" value="GROCERY" />
-              <Picker.Item label="Meat and Poultry" value="MEAT_AND_POULTRY" />
-              <Picker.Item label="Prepared Foods" value="PREPARED_FOODS" />
-              <Picker.Item label="Produce" value="PRODUCE" />
-              <Picker.Item label="Seafood" value="SEAFOOD" />
-              <Picker.Item label="Wine" value="WINE" />
-              <Picker.Item label="Whole Body" value="WHOLE_BODY" />
-              <Picker.Item label="Pets" value="PETS" />
-            </Picker>
-          </View>
-        </View>
-
-        <View style={{flexDirection: "row"}}>
-          <View style = {{flex: 1}}>
-            <Text style={styles.blackText}>Store Name: </Text>
-          </View>
-
-          <View style = {{flex: 1}}>
-            <TextInput
-              style={{height: 40, 
-                      borderColor: "gray", 
-                      borderWidth: 1,
-                      justifyContent: "flex-end"}}
-              onChangeText={(text) => this.setState({text})}
-              value={this.state.storeName}
-            />
-          </View>
-        </View>
-
-        <View style={{flexDirection: "row"}}>
-          <View style = {{flex: 1}}>
-            <Text style={styles.blackText}>Aisle Number: </Text>
-          </View>
-
-          <View style = {{flex: 1}}>
-            <TextInput
-              style={{height: 40, 
-                      borderColor: "gray", 
-                      borderWidth: 1,
-                      justifyContent: "flex-end"}}
-              keyboardType = "numeric"
-              onChangeText={(text) => this.setState({text})}
-              value={this.state.aisleNum}
-            />
-          </View>
+        <View style={styles.botContainer}>
+          <TouchableHighlight
+            style={[styles.buttonContainer, styles.submitButton]}
+            onPress={() => this.buttonListener()}
+          >
+            <Text style={styles.whiteText}>{"Register Item"}</Text>
+          </TouchableHighlight>
         </View>
       </View>
     );
--- Application/pages/pageStyles/AddItemPageStyle.js
+++ Application/pages/pageStyles/AddItemPageStyle.js
@@ -5,13 +5,68 @@ export default StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
-    color: "white",
+    backgroundColor: 'black',
+    color: "black",
+  },
+  topContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    color: "black",
+  },
+  midContainer: {
+    flex: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    color: "black",
+  },
+  botContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    color: "black",
+  },
+  whiteHeaderText: {
+    color: 'white',
+    fontSize: 20,
   },
   whiteText: {
     color: 'white',
   },
   blackText: {
     color: 'black',
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+  },
+  submitButton: {
+    backgroundColor: "#00b5ec",
+  },
+  textInput: {
+    height: 40, 
+    borderColor: "gray", 
+    borderWidth: 1,
+    justifyContent: "flex-end",
+    color: "white",
+  },
+  picker: {
+    height: 40, 
+    borderColor: "gray", 
+    borderWidth: 1,
+    justifyContent: "flex-end",
+    color: "white",
+  },
+  rowSorter: {
+    flexDirection: "row",
+    padding: 20,
   }
 });
