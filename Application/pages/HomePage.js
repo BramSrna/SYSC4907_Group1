d8b8d2cf94c32e169b60cfaff76c9008fc6d8d4e
Bram Srna <bramsrna@cmail.carleton.ca>
2019-09-04 18:34:06 -0400

    Move common styles to global

--- Application/pages/HomePage.js
+++ Application/pages/HomePage.js
@@ -3,6 +3,7 @@ import { Text,
          View,
          TouchableHighlight} from "react-native";
 import styles from "../pages/pageStyles/HomePageStyle";
+import globalStyles from "../pages/pageStyles/GlobalStyle";
 
 const ADD_ITEM_PAGE = "AddItemPage"
 
@@ -19,14 +20,14 @@ class HomePage extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.whiteText}>HomePage</Text>
+      <View style={globalStyles.defaultContainer}>
+        <Text style={globalStyles.whiteText}>HomePage</Text>
 
         <TouchableHighlight
-          style={[styles.buttonContainer, styles.addItemPageButton]}
+          style={[globalStyles.defaultButtonContainer, globalStyles.defaultButton]}
           onPress={() => this.buttonListener(ADD_ITEM_PAGE)}
         >
-          <Text style={styles.whiteText}>{"Go To Add Item Page"}</Text>
+          <Text style={globalStyles.whiteText}>{"Go To Add Item Page"}</Text>
         </TouchableHighlight>
 
       </View>
--- Application/pages/pageStyles/HomePageStyle.js
+++ Application/pages/pageStyles/HomePageStyle.js
@@ -1,25 +1,4 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#000000',
-  },
-  buttonContainer: {
-    height: 45,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 20,
-    width: 250,
-    borderRadius: 30,
-  },
-  addItemPageButton: {
-    backgroundColor: "#00b5ec",
-  },
-  whiteText: {
-    color: 'white'
-  }
 });
