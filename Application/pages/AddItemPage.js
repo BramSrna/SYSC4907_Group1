386127bf44c91470504b71bfb80bf4ab87353311
Haseeb <haseebakhan@cmail.carleton.ca>
2019-09-09 22:43:32 -0400

    Fix state issue

--- Application/pages/AddItemPage.js
+++ Application/pages/AddItemPage.js
@@ -1,147 +1,177 @@
 import React, { Component } from "react";
-import { Text,
-         View,
-         TextInput,
-         Picker,
-         Button,
-         Image,
-         TouchableHighlight,
-         Alert,
-         Platform,
-         KeyboardAvoidingView } from "react-native";
-import { Header } from "react-navigation"
+import {
+   Text,
+   View,
+   TextInput,
+   Picker,
+   Button,
+   Image,
+   TouchableHighlight,
+   Alert,
+   Platform,
+   KeyboardAvoidingView
+} from "react-native";
+import { Header } from "react-navigation";
 import styles from "./pageStyles/AddItemPageStyle";
 import globalStyles from "../pages/pageStyles/GlobalStyle";
-import {db} from "../config";
+import { db } from "../config";
 
-const keyboardVerticalOffset = Platform.OS === 'ios' ? (Header.HEIGHT + 64) : (Header.HEIGHT + 0)
-const keyboardAvoidingViewBehavior = Platform.OS === 'ios' ? "padding" : "padding"
+const keyboardVerticalOffset =
+   Platform.OS === "ios" ? Header.HEIGHT + 64 : Header.HEIGHT + 0;
+const keyboardAvoidingViewBehavior =
+   Platform.OS === "ios" ? "padding" : "padding";
 
 let addItem = (itemName, itemDepartment, storeName, aisleNum) => {
-  db.ref("/items").push({
-    name: itemName,
-    department: itemDepartment,
-    store: storeName,
-    aisleNum: aisleNum
-  });
+   db.database()
+      .ref("/items")
+      .push({
+         name: itemName,
+         department: itemDepartment,
+         store: storeName,
+         aisleNum: aisleNum
+      });
 };
 
 class AddItemPage extends Component {
-  constructor(props) {
-    super(props);
+   constructor(props) {
+      super(props);
 
-    this.state = {itemName: "",
-                  itemDepartment: "BAKERY",
-                  storeName: "",
-                  aisleNum: "",};
-  }
+      this.state = {
+         itemName: "",
+         itemDepartment: "BAKERY",
+         storeName: "",
+         aisleNum: ""
+      };
+   }
 
-  handleRegister = () => {
-    addItem(this.state.itemName,
-            this.state.itemDepartment,
-            this.state.storeName,
-            this.state.aisleNum);
-    Alert.alert("Item saved successfully");
-  };
+   handleRegister = () => {
+      addItem(
+         this.state.itemName,
+         this.state.itemDepartment,
+         this.state.storeName,
+         this.state.aisleNum
+      );
+      Alert.alert("Item saved successfully");
+   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.topContainer}>
-          <Text style={styles.whiteHeaderText}>Add Item:</Text>
-        </View>
-
-        <KeyboardAvoidingView
-          style={styles.midContainer}
-          keyboardVerticalOffset = {keyboardVerticalOffset}
-          behavior = {keyboardAvoidingViewBehavior}>
-          <View style={styles.rowSorter}>
-            <View style = {{flex: 1}}>
-              <Text style={globalStyles.whiteText}>Item Name: </Text>
+   render() {
+      return (
+         <View style={styles.container}>
+            <View style={styles.topContainer}>
+               <Text style={styles.whiteHeaderText}>Add Item:</Text>
             </View>
 
-            <View style = {{flex: 1}}>
-              <TextInput
-                style={styles.textInput}
-                placeholder="Item Name"
-                onChangeText={(itemName) => this.setState({itemName})}
-                value={this.state.itemName}
-              />
-            </View>
-          </View>
+            <KeyboardAvoidingView
+               style={styles.midContainer}
+               keyboardVerticalOffset={keyboardVerticalOffset}
+               behavior={keyboardAvoidingViewBehavior}
+            >
+               <View style={styles.rowSorter}>
+                  <View style={{ flex: 1 }}>
+                     <Text style={globalStyles.whiteText}>Item Name: </Text>
+                  </View>
 
-          <View style={styles.rowSorter}>
-            <View style = {{flex: 1}}>
-              <Text style={globalStyles.whiteText}>Item Department: </Text>
-            </View>
-            
-            <View style = {{flex: 1}}>
-              <Picker
-                selectedValue={this.state.itemDepartment}
-                style={styles.picker}
-                onValueChange={(itemDepartment) => this.setState({itemDepartment})
-                }>
-                <Picker.Item label="Bakery" value="BAKERY" />
-                <Picker.Item label="Beer" value="BEER" />
-                <Picker.Item label="Bulk" value="BULK" />
-                <Picker.Item label="Cheese" value="CHEESE" />
-                <Picker.Item label="Coffee And Tea" value="COFFEE_AND_TEA" />
-                <Picker.Item label="Flowers and Floral Arrangements" value="FLOWERS_AND_FLORAL_ARRANGEMENTS" />
-                <Picker.Item label="Grocery" value="GROCERY" />
-                <Picker.Item label="Meat and Poultry" value="MEAT_AND_POULTRY" />
-                <Picker.Item label="Prepared Foods" value="PREPARED_FOODS" />
-                <Picker.Item label="Produce" value="PRODUCE" />
-                <Picker.Item label="Seafood" value="SEAFOOD" />
-                <Picker.Item label="Wine" value="WINE" />
-                <Picker.Item label="Whole Body" value="WHOLE_BODY" />
-                <Picker.Item label="Pets" value="PETS" />
-              </Picker>
-            </View>
-          </View>
+                  <View style={{ flex: 1 }}>
+                     <TextInput
+                        style={styles.textInput}
+                        placeholder="Item Name"
+                        onChangeText={itemName => this.setState({ itemName })}
+                     />
+                     value={this.state.itemName}
+                  </View>
+               </View>
 
-          <View style={styles.rowSorter}>
-            <View style = {{flex: 1}}>
-              <Text style={globalStyles.whiteText}>Store Name: </Text>
-            </View>
+               <View style={styles.rowSorter}>
+                  <View style={{ flex: 1 }}>
+                     <Text style={globalStyles.whiteText}>
+                        Item Department:{" "}
+                     </Text>
+                  </View>
 
-            <View style = {{flex: 1}}>
-              <TextInput
-                style={styles.textInput}
-                placeholder="Store Name"
-                onChangeText={(storeName) => this.setState({storeName})}
-                value={this.state.storeName}
-              />
-            </View>
-          </View>
+                  <View style={{ flex: 1 }}>
+                     <Picker
+                        selectedValue={this.state.itemDepartment}
+                        style={styles.picker}
+                        onValueChange={itemDepartment =>
+                           this.setState({ itemDepartment })
+                        }
+                     >
+                        <Picker.Item label="Bakery" value="BAKERY" />
+                        <Picker.Item label="Beer" value="BEER" />
+                        <Picker.Item label="Bulk" value="BULK" />
+                        <Picker.Item label="Cheese" value="CHEESE" />
+                        <Picker.Item
+                           label="Coffee And Tea"
+                           value="COFFEE_AND_TEA"
+                        />
+                        <Picker.Item
+                           label="Flowers and Floral Arrangements"
+                           value="FLOWERS_AND_FLORAL_ARRANGEMENTS"
+                        />
+                        <Picker.Item label="Grocery" value="GROCERY" />
+                        <Picker.Item
+                           label="Meat and Poultry"
+                           value="MEAT_AND_POULTRY"
+                        />
+                        <Picker.Item
+                           label="Prepared Foods"
+                           value="PREPARED_FOODS"
+                        />
+                        <Picker.Item label="Produce" value="PRODUCE" />
+                        <Picker.Item label="Seafood" value="SEAFOOD" />
+                        <Picker.Item label="Wine" value="WINE" />
+                        <Picker.Item label="Whole Body" value="WHOLE_BODY" />
+                        <Picker.Item label="Pets" value="PETS" />
+                     </Picker>
+                  </View>
+               </View>
 
-          <View style={styles.rowSorter}>
-            <View style = {{flex: 1}}>
-              <Text style={globalStyles.whiteText}>Aisle Number: </Text>
-            </View>
+               <View style={styles.rowSorter}>
+                  <View style={{ flex: 1 }}>
+                     <Text style={globalStyles.whiteText}>Store Name: </Text>
+                  </View>
 
-            <View style = {{flex: 1}}>
-              <TextInput
-                style={styles.textInput}                
-                placeholder="Aisle Number"
-                keyboardType = "numeric"
-                onChangeText={(aisleNum) => this.setState({aisleNum})}
-                value={this.state.aisleNum}                
-              />
-            </View>
-          </View>
-        </KeyboardAvoidingView>
+                  <View style={{ flex: 1 }}>
+                     <TextInput
+                        style={styles.textInput}
+                        placeholder="Store Name"
+                        onChangeText={storeName => this.setState({ storeName })}
+                        value={this.state.storeName}
+                     />
+                  </View>
+               </View>
 
-        <View style={styles.botContainer}>
-          <TouchableHighlight
-            style={[globalStyles.defaultButtonContainer, globalStyles.defaultButton]}
-            onPress={this.handleRegister}
-          >
-            <Text style={globalStyles.whiteText}>{"Register Item"}</Text>
-          </TouchableHighlight>
-        </View>
-      </View>
-    );
-  }
+               <View style={styles.rowSorter}>
+                  <View style={{ flex: 1 }}>
+                     <Text style={globalStyles.whiteText}>Aisle Number: </Text>
+                  </View>
+
+                  <View style={{ flex: 1 }}>
+                     <TextInput
+                        style={styles.textInput}
+                        placeholder="Aisle Number"
+                        keyboardType="numeric"
+                        onChangeText={aisleNum => this.setState({ aisleNum })}
+                        value={this.state.aisleNum}
+                     />
+                  </View>
+               </View>
+            </KeyboardAvoidingView>
+
+            <View style={styles.botContainer}>
+               <TouchableHighlight
+                  style={[
+                     globalStyles.defaultButtonContainer,
+                     globalStyles.defaultButton
+                  ]}
+                  onPress={this.handleRegister}
+               >
+                  <Text style={globalStyles.whiteText}>{"Register Item"}</Text>
+               </TouchableHighlight>
+            </View>
+         </View>
+      );
+   }
 }
 
 export default AddItemPage;
--- Application/pages/YourLists.js
+++ Application/pages/YourLists.js
@@ -1,109 +1,163 @@
 import React, { Component } from "react";
 import { Text, View, FlatList, Alert, BackHandler } from "react-native";
 import styles from "./pageStyles/YourListsPageStyle";
-
-/**
- *  TODO
- On this page we will need to get all the titles and actual items that the user is a part of. We will need the Cloud API to do this. For now I have hardcoded data to move on but I will need to come back to this once the Cloud is all setup.
- */
+import { db } from "../config";
 
 class YourLists extends Component {
-  constructor(props) {
-    super(props);
+   constructor(props) {
+      super(props);
 
-    this.state = {
-      listTitles: [],
-      apiData: []
-    };
-  }
+      this.state = {
+         listTitles: [],
+         apiData: []
+      };
+   }
 
-  componentDidMount() {
-    // Im guessing this is something similar to what the Firebase query will return
-    this.setState(
-      {
-        apiData: [
-          { name: "List One", items: ["Item1", "Item2"] },
-          { name: "List Two", items: ["Item1", "Item2", "Item3"] }
-        ]
-      },
-      function() {
-        this.GenerateListTitlesFromApiData();
+   componentDidMount() {
+      var listIds = [];
+      var uid = /*db.auth().currentUser.uid*/ "Tbn0C7WvyRSoQZ9KQNjFPjIfmGJ2";
+      db.database()
+         .ref("/users/" + uid + "/lists")
+         .once("value")
+         .then(function(snapshot) {
+            var ssv = snapshot.val();
+            if (ssv) {
+               for (var createdKey in ssv.created) {
+                  listIds.push(ssv.created[createdKey]);
+               }
+               for (var sharedWithKey in ssv.shared_with) {
+                  listIds.push(ssv.shared_with[sharedWithKey]);
+               }
+               for (var idKey in listIds) {
+                  var bool = false;
+                  db.database()
+                     .ref("/lists/" + listIds[idKey])
+                     .once("value")
+                     .then(function(snapshot) {
+                        var ssv = snapshot.val();
+                        if (ssv) {
+                           var listItems = [];
+                           for (var item in ssv.items) {
+                              listItems.push(ssv.items[item]);
+                           }
+                           var curApiData = this.state.apiData;
+                           curApiData.push({
+                              name: ssv.name,
+                              items: listItems
+                           });
+                           this.setState(
+                              {
+                                 apiData: curApiData
+                              },
+                              function() {
+                                 this.GenerateListTitlesFromApiData();
+                              }
+                           );
+                        } else {
+                           console.log("ERROR: List does not exist.");
+                           Alert.alert("ERROR: List does not exist.");
+                           this.setState(
+                              {
+                                 apiData: []
+                              },
+                              function() {
+                                 this.GenerateListTitlesFromApiData();
+                              }
+                           );
+                           bool = true;
+                        }
+                     });
+                  if (bool) break;
+               }
+            } else {
+               this.setState(
+                  {
+                     apiData: []
+                  },
+                  function() {
+                     this.GenerateListTitlesFromApiData();
+                  }
+               );
+            }
+         });
+
+      BackHandler.addEventListener("hardwareBackPress", function() {
+         // Return true if you want to go back, false if want to ignore. This is for Android only.
+         // return true;
+         return false;
+      });
+   }
+
+   GenerateListTitlesFromApiData() {
+      var data = this.state.apiData;
+      var tempNames = [];
+      for (var list in data) {
+         tempNames.push(data[list].name);
       }
-    );
-    BackHandler.addEventListener("hardwareBackPress", function() {
-      // Return true if you want to go back, false if want to ignore. This is for Android only.
-      // return true;
-      return false;
-    });
-  }
+      this.setState({ listTitles: tempNames });
+   }
 
-  GenerateListTitlesFromApiData() {
-    var data = this.state.apiData;
-    var tempNames = [];
-    for (var list in data) {
-      tempNames.push(data[list].name);
-    }
-    this.setState({ listTitles: tempNames });
-  }
-
-  GenerateListItemsFromApiData(listName) {
-    var data = this.state.apiData;
-    var tempList = [];
-    for (var list in data) {
-      if (data[list].name == listName) {
-        tempList = data[list].items;
+   GenerateListItemsFromApiData(listName) {
+      var data = this.state.apiData;
+      var tempList = [];
+      for (var list in data) {
+         if (data[list].name == listName) {
+            tempList = data[list].items;
+         }
       }
-    }
-    return tempList;
-  }
+      return tempList;
+   }
 
-  /**
+   /**
    * TODO
    Should we pass in all the API data, so that way if they return to the page we don't have to make another API call.
    Pro: Less API call --> Faster? Save data?
    Con: Passing in a lot of useless info to another page --> Slower?
    */
-  GoToList(item) {
-    this.props.navigation.navigate("Current List", {
-      name: item,
-      list: this.GenerateListItemsFromApiData(item)
-    });
-  }
+   GoToList(item) {
+      this.props.navigation.navigate("Current List", {
+         name: item,
+         list: this.GenerateListItemsFromApiData(item)
+      });
+   }
 
-  FlatListItemSeparator = () => {
-    return (
-      <View
-        style={{
-          height: 1,
-          width: "100%",
-          backgroundColor: "#607D8B"
-        }}
-      />
-    );
-  };
+   FlatListItemSeparator = () => {
+      return (
+         <View
+            style={{
+               height: 1,
+               width: "100%",
+               backgroundColor: "#607D8B"
+            }}
+         />
+      );
+   };
 
-  render() {
-    return (
-      <View style={styles.ListContainer}>
-        <Text style={styles.pageTitle}>
-          Your Lists: {this.state.listTitles.length}
-        </Text>
-        <FlatList
-          style={styles.flatList}
-          data={this.state.listTitles}
-          width="100%"
-          extraData={this.state.arrayHolder}
-          keyExtractor={index => index.toString()}
-          ItemSeparatorComponent={this.FlatListItemSeparator}
-          renderItem={({ item }) => (
-            <Text style={styles.item} onPress={this.GoToList.bind(this, item)}>
-              {item}
+   render() {
+      return (
+         <View style={styles.ListContainer}>
+            <Text style={styles.pageTitle}>
+               Your Lists: {this.state.listTitles.length}
             </Text>
-          )}
-        />
-      </View>
-    );
-  }
+            <FlatList
+               style={styles.flatList}
+               data={this.state.listTitles}
+               width="100%"
+               extraData={this.state.arrayHolder}
+               keyExtractor={index => index.toString()}
+               ItemSeparatorComponent={this.FlatListItemSeparator}
+               renderItem={({ item }) => (
+                  <Text
+                     style={styles.item}
+                     onPress={this.GoToList.bind(this, item)}
+                  >
+                     {item}
+                  </Text>
+               )}
+            />
+         </View>
+      );
+   }
 }
 
 export default YourLists;
