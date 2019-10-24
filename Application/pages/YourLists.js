48e6d10d7f757a300e773de69d1e17713fa6cfe4
Haseeb <haseebakhan@cmail.carleton.ca>
2019-10-23 22:38:26 -0400

    Fixed error indentation

--- Application/pages/YourLists.js
+++ Application/pages/YourLists.js
@@ -89,138 +89,75 @@ class YourLists extends Component {
 
    render() {
       const swipeButtons = [{
-         component: (<
-            View style={
-               {
-                  flex: 1,
-                  alignItems: "center",
-                  justifyContent: "center",
-                  flexDirection: "column"
-               }
-            } >
+         component: (<View style={
+            {
+               flex: 1,
+               alignItems: "center",
+               justifyContent: "center",
+               flexDirection: "column"
+            }
+         } >
             <
                Image source={
                   require("../images/delete_list_button.png")
                }
-            /> < /
-            View >
+            /> </View >
          ),
          backgroundColor: "red",
          onPress: () => {
             var listId = this.state.apiData[this.state.activeRow].key;
          }
       }];
-      return ( <
-         View style={
-                  styles.ListContainer
-               } >
-               <
-         Dialog.Container visible={
-                     this.state.isDialogVisible
-                  }
-                  style={
-                     {}
-                  } >
-                  <
-         Dialog.Title > New List < /Dialog.Title> <
-         Dialog.Description >
-                        Enter the name of the new list you would like to create:
-         <
-         /Dialog.Description> <
-         Dialog.Input onChangeText={
-                              name => this.setNewListName(name)
-                           } >
-                           <
-         /Dialog.Input> <
-                              Dialog.Button label="Cancel"
-                              onPress={
-                                 this.handleCancel
-                              }
-                           /> <
-                              Dialog.Button label="Create"
-                              onPress={
-                                 this.handleCreate
-                              }
-                           /> < /
-         Dialog.Container > <
-         Text style={
-                                 styles.pageTitle
-                              } >
-                              Your Lists: {
-                                 this.state.listTitles.length
-                              } <
-         /Text> <
-         TouchableOpacity onPress={
-                                    () => this.setState({
-                                       isDialogVisible: true
-                                    })
-                                 } >
-                                 <
-                                    Image source={
-                                       require("../images/new_list_button.png")
-                                    }
-                                 /> < /
-         TouchableOpacity > <
-                                    FlatList style={
-                                       styles.flatList
-                                    }
-                                    data={
-                                       this.state.listTitles
-                                    }
-                                    width="100%"
-                                    extraData={
-                                       this.state.activeRow
-                                    }
-                                    keyExtractor={
-                                       index => index.toString()
-                                    }
-                                    ItemSeparatorComponent={
-                                       this.FlatListItemSeparator
-                                    }
-                                    renderItem={
-                                       ({
-                                          item,
-                                          index
-                                       }) => (<
-               Swipeout right={
-                                             swipeButtons
-                                          }
-                                          backgroundColor="#000000"
-                                          underlayColor="white"
-                                          rowID={
-                                             index
-                                          }
-                                          sectionId={
-                                             1
-                                          }
-                                          autoClose={
-                                             true
-                                          }
-                                          onOpen={
-                                             (secId, rowId, direction) =>
-                                                this.handleSwipeOpen(rowId, direction)
-                                          }
-                                          close={
-                                             this.state.activeRow !== index
-                                          } >
-                                          <
-               TouchableOpacity onPress={
-                                                this.GoToList.bind(this, item)
-                                             } >
-                                             <
-               Text style={
-                                                   styles.item
-                                                } > {
-                                                   item
-                                                } < /Text> < /
-               TouchableOpacity > <
-               /Swipeout>
-                                             )
-                                          }
-         /> < /
-         View >
-                                             );
-                                          }
-                                       }
-                                       
+      return (
+         <View style={styles.ListContainer}>
+            <Dialog.Container visible={this.state.isDialogVisible} style={{}}>
+               <Dialog.Title>New List</Dialog.Title>
+               <Dialog.Description>
+                  Enter the name of the new list you would like to create:
+               </Dialog.Description>
+               <Dialog.Input
+                  onChangeText={name => this.setNewListName(name)}
+               ></Dialog.Input>
+               <Dialog.Button label="Cancel" onPress={this.handleCancel} />
+               <Dialog.Button label="Create" onPress={this.handleCreate} />
+            </Dialog.Container>
+            <Text style={styles.pageTitle}>
+               Your Lists: {this.state.listTitles.length}
+            </Text>
+            <TouchableOpacity
+               onPress={() => this.setState({ isDialogVisible: true })}
+            >
+               <Image source={require("../images/new_list_button.png")} />
+            </TouchableOpacity>
+            <FlatList
+               style={styles.flatList}
+               data={this.state.listTitles}
+               width="100%"
+               extraData={this.state.activeRow}
+               keyExtractor={index => index.toString()}
+               ItemSeparatorComponent={this.FlatListItemSeparator}
+               renderItem={({ item, index }) => (
+                  <Swipeout
+                     right={swipeButtons}
+                     backgroundColor="#000000"
+                     underlayColor="white"
+                     rowID={index}
+                     sectionId={1}
+                     autoClose={true}
+                     onOpen={(secId, rowId, direction) =>
+                        this.handleSwipeOpen(rowId, direction)
+                     }
+                     close={this.state.activeRow !== index}
+                  >
+                     <TouchableOpacity onPress={this.GoToList.bind(this, item)}>
+                        <Text style={styles.item}>{item}</Text>
+                     </TouchableOpacity>
+                  </Swipeout>
+               )}
+            />
+         </View>
+      );
+   }
+}
+
 export default YourLists;
