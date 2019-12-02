bece5cb62af66cc3965751139f87f370d0b32752
Jobin Mathew <jobinmatt@live.ca>
2019-12-02 00:08:11 -0500

    Register page is functional

--- Application/pages/RegisterItemPage.js
+++ Application/pages/RegisterItemPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Text, View, TextInput, TouchableHighlight, Alert, Platform, KeyboardAvoidingView, StyleSheet} from "react-native";
-import { Layout, Button, Input, Icon, TopNavigation, TopNavigationAction } from 'react-native-ui-kitten';
+import { Text, View, TextInput, TouchableHighlight, Alert, Platform, KeyboardAvoidingView, StyleSheet } from "react-native";
+import { Layout, Button, Input, Select, Icon, TopNavigation, TopNavigationAction } from 'react-native-ui-kitten';
 import { MenuOutline } from "../assets/icons/icons.js";
 import { ScrollView } from "react-native-gesture-handler";
 import { dark, light } from '../assets/Themes.js';
@@ -20,7 +20,7 @@ const PAGE_TITLE = "Register Item";
 const DEFAULT_GENERIC_NAME = ""
 const DEFAULT_SPECIFIC_NAME = ""
 const DEFAULT_SIZE = ""
-const DEFAULT_SIZE_UNIT = "Please select the size unit..."
+const DEFAULT_SIZE_UNIT = ""
 
 class RegisterItemPage extends Component {
   constructor(props) {
@@ -127,89 +127,43 @@ class RegisterItemPage extends Component {
           alignment="center"
           leftControl={this.renderMenuAction()}
         />
-        <ScrollView style={[styles.scrollContainer, { backgroundColor: global.theme == light ? light["background-basic-color-1"] : dark["background-basic-color-1"] }]}>
-          <Layout>
-
-          </Layout>
-        </ScrollView>
-        <View style={styles.container}>
-          <View style={styles.topContainer}>
-            <Text style={styles.whiteHeaderText}>Register Item</Text>
-          </View>
-
-          <KeyboardAvoidingView
-            style={styles.midContainer}
-            keyboardVerticalOffset={keyboardVerticalOffset}
-            behavior={keyboardAvoidingViewBehavior}>
-            <View style={styles.rowSorter}>
-              <View style={{ flex: 1 }}>
-                {this.renderRequiredText("Generic Name: ")}
-              </View>
-
-              <View style={{ flex: 1 }}>
-                <TextInput
-                  style={styles.textInput}
-                  placeholder="Generic Name Ex. Ketchup"
-                  onChangeText={(genericName) => this.setState({ genericName })}
+        <KeyboardAvoidingView style={[styles.avoidingView, { backgroundColor: global.theme == light ? light["background-basic-color-1"] : dark["background-basic-color-1"] }]} behavior="padding" enabled keyboardVerticalOffset={24}>
+          <ScrollView style={[styles.scrollContainer, { backgroundColor: global.theme == light ? light["background-basic-color-1"] : dark["background-basic-color-1"] }]}>
+            <Layout style={styles.formOuterContainer} level='3'>
+              <Layout style={styles.formInnerContainer}>
+                <Input style={styles.inputRow}
+                  label='Generic Name'
+                  placeholder='Ex. Ketchup'
                   value={this.state.genericName}
+                  onChangeText={(genericName) => this.setState({ genericName })}
                 />
-              </View>
-            </View>
-
-            <View style={styles.rowSorter}>
-              <View style={{ flex: 1 }}>
-                {this.renderRequiredText("Specific Name: ")}
-              </View>
-
-              <View style={{ flex: 1 }}>
-                <TextInput
-                  style={styles.textInput}
-                  placeholder="Specific Name"
-                  onChangeText={(specificName) => this.setState({ specificName })}
+                <Input style={styles.inputRow}
+                  label='Specific Name'
+                  placeholder='Specific Name'
                   value={this.state.specificName}
+                  onChangeText={(specificName) => this.setState({ specificName })}
                 />
-              </View>
-            </View>
-
-            <View style={styles.rowSorter}>
-              <View style={{ flex: 1 }}>
-                <Text style={globalStyles.whiteText}>Size: </Text>
-              </View>
-
-              <View style={{ flex: 1 }}>
-                <TextInput
-                  style={styles.textInput}
-                  placeholder="Size"
-                  keyboardType="numeric"
-                  onChangeText={(size) => this.setState({ size })}
-                  value={this.state.size}
-                />
-              </View>
-
-              <View style={{ flex: 0.1 }} />
-
-              <View style={{ flex: 1 }}>
-                <RNPickerSelect
-                  value={this.state.sizeUnit}
-                  items={units}
-                  style={pickerStyle}
-                  useNativeAndroidPickerStyle={false}
-                  placeholder={{}}
-                  onValueChange={(sizeUnit) => this.setState({ sizeUnit })} />
-              </View>
-            </View>
-          </KeyboardAvoidingView>
-
-          <View style={styles.botContainer}>
-            <TouchableHighlight
-              style={[globalStyles.defaultButtonContainer, globalStyles.defaultButton]}
-              onPress={this.handleRegister}
-            >
-              <Text style={globalStyles.whiteText}>{"Register Item"}</Text>
-            </TouchableHighlight>
-          </View>
-        </View>
-      </React.Fragment>
+                <Layout style={styles.horizontalInnerContainer}>
+                  <Input style={styles.inputLeftColumn}
+                    label='Size'
+                    placeholder='Size'
+                    keyboardType='numeric'
+                    value={this.state.size}
+                    onChangeText={(size) => this.setState({ size })}
+                  />
+                  <Select style={styles.inputRightColumn}
+                    data={units}
+                    placeholder='Unit'
+                    selectedOption={this.state.sizeUnit}
+                    onSelect={(sizeUnit) => this.setState({ sizeUnit })}
+                  />
+                </Layout>
+                <Button style={styles.button} onPress={this.handleRegister} >Register Item</Button>
+              </Layout>
+            </Layout>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </React.Fragment >
     );
   }
 }
@@ -224,6 +178,11 @@ const styles = StyleSheet.create({
   scrollContainer: {
     flex: 1,
   },
+  avoidingView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
   overflowMenu: {
     padding: 4,
     shadowColor: 'black',
@@ -231,74 +190,40 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 0 },
     elevation: 8,
   },
-
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black',
-    color: "black",
+  formOuterContainer: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 10,
   },
-  topContainer: {
+  formInnerContainer: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 8,
     alignItems: 'center',
-    backgroundColor: 'black',
-    color: "black",
+    justifyContent: 'center',
+    borderRadius: 10,
   },
-  midContainer: {
+  inputRow: {
+    paddingVertical: 4,
+  },
+  horizontalInnerContainer: {
+    flexDirection: 'row',
+  },
+  inputLeftColumn: {
     flex: 4,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black',
-    color: "black",
+    paddingVertical: 4,
   },
-  botContainer: {
+  inputRightColumn: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black',
-    color: "black",
+    paddingTop: 21,
+    paddingLeft: 8,
+    paddingVertical: 4,
+    minWidth: 60,
   },
-  whiteHeaderText: {
-    color: 'white',
-    fontSize: 20,
+  button: {
+    flex: 1,
+    marginTop: 8,
+    width: '100%',
   },
-  blackText: {
-    color: 'black',
-  },
-  textInput: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    justifyContent: "flex-end",
-    color: "white",
-  },
-  rowSorter: {
-    flexDirection: "row",
-    padding: 20,
-  }
 });
-
-export const pickerStyle = {
-  inputAndroid: {
-    height: 30,
-    borderColor: "gray",
-    borderWidth: 1,
-    justifyContent: "flex-end",
-    color: "white",
-    paddingRight: 30,
-    width: 80
-  },
-  inputIOS: {
-    height: 30,
-    borderColor: "gray",
-    borderWidth: 1,
-    justifyContent: "flex-end",
-    color: "white",
-    paddingRight: 30,
-    width: 80
-  },
-};
 
 export default RegisterItemPage;
