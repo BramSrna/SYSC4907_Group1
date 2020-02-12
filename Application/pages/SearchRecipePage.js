68b5f0cf0b088b4653171076a0d21d70781d2155
Haseeb <haseebakhan@cmail.carleton.ca>
2020-02-11 20:05:57 -0500

    API works

--- Application/pages/SearchRecipePage.js
+++ Application/pages/SearchRecipePage.js
@@ -1,19 +1,14 @@
+import * as firebase from "firebase";
 import React, { Component } from 'react';
 import { Button } from 'react-native-ui-kitten';
-import { WebView } from 'react-native-webview';
 import NotificationPopup from 'react-native-push-notification-popup';
 import nm from '../pages/Functions/NotificationManager.js';
-const HTMLParser = require('fast-html-parser');
+const API_KEY = "5c39c2c2fa5e4183a1f9a3f799e49baa"; // 50/1.01 calls/day allowed
 
 export default class SearchRecipePage extends Component {
 
   constructor(props) {
     super(props);
-    this.WEBVIEW_REF = React.createRef();
-    this.state = {
-      canGoBack: false,
-      url: ''
-    };
   }
 
   componentWillMount() {
@@ -32,43 +27,36 @@ export default class SearchRecipePage extends Component {
     this._isMounted = false;
   }
 
-  onNavigationStateChange(navState) {
-    this.setState({
-      canGoBack: navState.canGoBack,
-      url: navState.url
-    });
+  getRecipe = () => {
+    // let url = "https://api.spoonacular.com/recipes/random?number=1&apiKey=" + API_KEY;
+    // fetch(url, {
+    //   method: "GET",
+    // }).then(function (response) {
+    //   if (response.status === 200) {
+
+    //     response.json().then(function (json) {
+    //       const title = json.recipes[0].title
+    //       const data = json.recipes[0];
+    //       firebase.database().ref('/recipes/' + title).set(data).then(function (snapshot) {
+    //         // console.log(snapshot);
+    //       });
+    //     });
+
+    //   } else {
+    //     console.log("API did not respond well.")
+    //   }
+    // }, function (error) {
+    //   console.log(error.message)
+    // })
+    console.log("getRecipe is commented out to save API calls")
   }
 
-  handleBackButton = () => {
-    this.WEBVIEW_REF.current.goBack();
-    return true;
-  }
-
-  scanForIngredients = async () => {
-    const url = this.state.url;
-    const response = await fetch(url);
-    const html = await response.text();
-    const root = HTMLParser.parse(html);
-    this.findIngredients(root.querySelectorAll('li.checkList__line'))
-  }
-
-  findIngredients = (qs) => {
-    console.log(qs.length)
-    for (var a = 0; a < qs.length - 3; a++)
-      console.log(a + qs[a].childNodes[1].childNodes[3].childNodes[0].rawText)
-  }
 
   render() {
     return (
       <React.Fragment>
-        <Button disabled={false} onPress={this.handleBackButton}>Go Back</Button>
-        <Button disabled={false} onPress={this.scanForIngredients}>READ</Button>
-        <WebView
-          ref={this.WEBVIEW_REF}
-          source={{ uri: 'https://www.allrecipes.com/recipe/21513/country-sausage-gravy/?internalSource=rotd&referringId=78&referringContentType=Recipe%20Hub' }}
-          style={{ marginTop: 20 }}
-          onNavigationStateChange={this.onNavigationStateChange.bind(this)}
-        />
+        <Button disabled={false} onPress={this.getRecipe}>GET A RECIPE</Button>
+
         <NotificationPopup ref={ref => this.popup = ref} />
       </React.Fragment>
     );
