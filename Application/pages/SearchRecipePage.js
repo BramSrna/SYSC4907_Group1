23993938a7b5b69b4c1f1c386439d1446713f20d
Haseeb <haseebakhan@cmail.carleton.ca>
2020-02-12 19:30:42 -0500

    Progress:

--- Application/pages/SearchRecipePage.js
+++ Application/pages/SearchRecipePage.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import NotificationPopup from 'react-native-push-notification-popup';
 import nm from '../pages/Functions/NotificationManager.js';
+import rf from "./Functions/RecipeFunctions";
+import { ScrollView, StyleSheet } from 'react-native';
+import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-cards';
 
 export default class SearchRecipePage extends Component {
 
@@ -27,9 +30,88 @@ export default class SearchRecipePage extends Component {
   render() {
     return (
       <React.Fragment>
-
+        <ScrollView style={styles.card}>
+          <Card>
+            <CardImage
+              source={{ uri: 'http://bit.ly/2GfzooV' }}
+            // title="Top 10 South African beaches"
+            />
+            <CardTitle
+              subtitle="Number 6"
+            />
+            <CardContent text="Clifton, Western Cape" />
+            <CardAction
+              separator={true}
+              inColumn={false}>
+              <CardButton
+                onPress={() => { }}
+                title="Share"
+                color="#FEB557"
+              />
+              <CardButton
+                onPress={() => { }}
+                title="Explore"
+                color="#FEB557"
+              />
+            </CardAction>
+          </Card>
+          <Card>
+            <CardImage
+              source={{ uri: 'http://bit.ly/2GfzooV' }}
+            // title="Top 10 South African beaches"
+            />
+            <CardTitle
+              subtitle="Number 6"
+            />
+            <CardContent text="Clifton, Western Cape" />
+            <CardAction
+              separator={true}
+              inColumn={false}>
+              <CardButton
+                onPress={() => { }}
+                title="Share"
+                color="#FEB557"
+              />
+              <CardButton
+                onPress={() => { }}
+                title="Explore"
+                color="#FEB557"
+              />
+            </CardAction>
+          </Card>
+          <Card>
+            <CardImage
+              source={{ uri: 'http://bit.ly/2GfzooV' }}
+            // title="Top 10 South African beaches"
+            />
+            <CardTitle
+              subtitle="Number 6"
+            />
+            <CardContent text="Clifton, Western Cape" />
+            <CardAction
+              separator={true}
+              inColumn={false}>
+              <CardButton
+                onPress={() => { }}
+                title="Share"
+                color="#FEB557"
+              />
+              <CardButton
+                onPress={() => { }}
+                title="Explore"
+                color="#FEB557"
+              />
+            </CardAction>
+          </Card>
+        </ScrollView>
         <NotificationPopup ref={ref => this.popup = ref} />
       </React.Fragment>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'black'
+  }
+});
