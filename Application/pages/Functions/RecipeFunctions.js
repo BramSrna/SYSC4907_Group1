36005734f20fd37c6327c26d2af00b67825125cf
Haseeb <haseebakhan@cmail.carleton.ca>
2020-02-12 19:13:49 -0500

    Beginnging

---
+++ Application/pages/Functions/RecipeFunctions.js
@@ -0,0 +1,34 @@
+import * as firebase from "firebase";
+
+const NUMBER_OF_RECIPES_TO_GET_FROM_API_TO_STORE_IN_DB = 1;
+const API_KEY = "5c39c2c2fa5e4183a1f9a3f799e49baa"; // 50/1.01 calls/day allowed
+
+class RecipeFunctions {
+   constructor() {}
+
+   AddRecipesToDatabase() {
+      let url = "https://api.spoonacular.com/recipes/random?number=" + NUMBER_OF_RECIPES_TO_GET_FROM_API_TO_STORE_IN_DB + "&apiKey=" + API_KEY;
+      fetch(url, {
+         method: "GET",
+      }).then(function (response) {
+         if (response.status === 200) {
+
+            response.json().then(function (json) {
+               const title = json.recipes[0].title
+               const data = json.recipeConts[0];
+               firebase.database().ref('/recipes/' + title).set(data).then(function (snapshot) {
+                  // console.log(snapshot);
+               });
+            });
+
+         } else {
+            console.log("API did not respond well.")
+         }
+      }, function (error) {
+         console.log(error.message)
+      })
+   }
+}
+
+const rf = new RecipeFunctions();
+export default rf;
--- Application/pages/SearchRecipePage.js
+++ Application/pages/SearchRecipePage.js
@@ -1,9 +1,6 @@
-import * as firebase from "firebase";
 import React, { Component } from 'react';
-import { Button } from 'react-native-ui-kitten';
 import NotificationPopup from 'react-native-push-notification-popup';
 import nm from '../pages/Functions/NotificationManager.js';
-const API_KEY = "5c39c2c2fa5e4183a1f9a3f799e49baa"; // 50/1.01 calls/day allowed
 
 export default class SearchRecipePage extends Component {
 
@@ -27,35 +24,9 @@ export default class SearchRecipePage extends Component {
     this._isMounted = false;
   }
 
-  getRecipe = () => {
-    // let url = "https://api.spoonacular.com/recipes/random?number=1&apiKey=" + API_KEY;
-    // fetch(url, {
-    //   method: "GET",
-    // }).then(function (response) {
-    //   if (response.status === 200) {
-
-    //     response.json().then(function (json) {
-    //       const title = json.recipes[0].title
-    //       const data = json.recipes[0];
-    //       firebase.database().ref('/recipes/' + title).set(data).then(function (snapshot) {
-    //         // console.log(snapshot);
-    //       });
-    //     });
-
-    //   } else {
-    //     console.log("API did not respond well.")
-    //   }
-    // }, function (error) {
-    //   console.log(error.message)
-    // })
-    console.log("getRecipe is commented out to save API calls")
-  }
-
-
   render() {
     return (
       <React.Fragment>
-        <Button disabled={false} onPress={this.getRecipe}>GET A RECIPE</Button>
 
         <NotificationPopup ref={ref => this.popup = ref} />
       </React.Fragment>
