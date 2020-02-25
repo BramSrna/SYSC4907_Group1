54cdfc471f4538fd5d63e37c62368f2e906b2559
Haseeb <haseebakhan@cmail.carleton.ca>
2020-02-24 19:28:52 -0500

    Change to FlatList

--- Application/pages/FindRecipePage.js
+++ Application/pages/FindRecipePage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import NotificationPopup from 'react-native-push-notification-popup';
 import nm from './Functions/NotificationManager.js';
 import rf from "./Functions/RecipeFunctions";
-import { ScrollView, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-cards';
 import { TopNavigation, TopNavigationAction, } from 'react-native-ui-kitten';
 import { MenuOutline } from "../assets/icons/icons.js";
@@ -43,53 +43,52 @@ export default class FindRecipePage extends Component {
           alignment='center'
           leftControl={renderMenuAction()}
         />
-        <ScrollView style={styles.card}>
-          {
-            this.state.recipes.map((aRecipe) => {
-              {
-                if ({ aRecipe }) {
-                  return (
-                    <Card key={"Key: " + aRecipe.title}>
-                      <CardImage
-                        source={{ uri: aRecipe.image }}
-                      // title="Top 10 South African beaches"
-                      />
-                      <CardTitle
-                        subtitle={aRecipe.title}
-                      />
-                      <CardContent text={"Serves " + aRecipe.servings + "\t\t\tReady in  " + aRecipe.readyInMinutes + " minutes"} />
-                      <CardAction
-                        separator={true}
-                        inColumn={false}>
-                        <CardButton
-                          onPress={() => {
-                            this.props.navigation.navigate("YourContacts", {
-                              share: true,
-                              recipeName: aRecipe.title,
-                              recipeUrl: aRecipe.spoonacularSourceUrl
-                            })
-                          }}
-                          title="Share"
-                          color="#FEB557"
-                        />
-                        <CardButton
-                          onPress={() => {
-                            this.props.navigation.navigate("RecipeDetailsPage", {
-                              url: aRecipe.spoonacularSourceUrl,
-                              ingredients: aRecipe.extendedIngredients
-                            })
-                          }}
-                          title="Details"
-                          color="#FEB557"
-                        />
-                      </CardAction>
-                    </Card>
-                  );
-                }
-              }
-            })
-          }
-        </ScrollView>
+        {this.state.recipes.length > 0 &&
+          <FlatList
+            contentContainerStyle={{ paddingBottom: 16 }}
+            style={styles.flatList}
+            data={this.state.recipes}
+            width="100%"
+            keyExtractor={item => item.title}
+            renderItem={({ item }) => {
+              return (
+                <Card key={"Key: " + item.title}>
+                  <CardImage
+                    source={{ uri: item.image }}
+                  />
+                  <CardTitle
+                    subtitle={item.title}
+                  />
+                  <CardContent text={"Serves " + item.servings + "\t\t\tReady in  " + item.readyInMinutes + " minutes"} />
+                  <CardAction
+                    separator={true}
+                    inColumn={false}>
+                    <CardButton
+                      onPress={() => {
+                        this.props.navigation.navigate("YourContacts", {
+                          share: true,
+                          recipeName: item.title,
+                          recipeUrl: item.spoonacularSourceUrl
+                        })
+                      }}
+                      title="Share"
+                      color="#FEB557"
+                    />
+                    <CardButton
+                      onPress={() => {
+                        this.props.navigation.navigate("RecipeDetailsPage", {
+                          url: item.spoonacularSourceUrl,
+                          ingredients: item.extendedIngredients
+                        })
+                      }}
+                      title="Details"
+                      color="#FEB557"
+                    />
+                  </CardAction>
+                </Card>
+              );
+            }}
+          />}
         <NotificationPopup ref={ref => this.popup = ref} />
       </React.Fragment>
     );
