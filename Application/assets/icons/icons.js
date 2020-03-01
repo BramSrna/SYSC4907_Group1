e180dcbe5ed4c07312373d4ae01d9b4729fb68d3
jobinmatt <jobinmatt@live.ca>
2020-03-01 02:34:54 -0500

    Removed unused assets

--- Application/assets/icons/icons.js
+++ Application/assets/icons/icons.js
@@ -1,20 +1,5 @@
 import React from 'react';
-import { Image } from 'react-native';
 import { Icon } from 'react-native-ui-kitten';
-
-export const EmailIcon = (style) => (
-    <Image
-        style={style}
-        source={require("./icons8-mail-account-64.png")}
-    />
-);
-
-export const PasswordIcon = (style) => (
-    <Image
-        style={style}
-        source={require("./icons8-key-64.png")}
-    />
-);
 
 export const MenuOutline = (style) => (
     <Icon {...style} name='menu-outline' />
